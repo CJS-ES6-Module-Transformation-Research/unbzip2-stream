cb93fa9b0bd06f7fb43bb466eb8132c4988b2b43
sam raphael <samraphael@gmail.com>
2021-08-06 14:52:35 -0400

    pre-processing with update

--- test/extra/long.js
+++ test/extra/long.js
@@ -1,17 +1,16 @@
-var unbzip2Stream = require('../..');
+var unbzip2Stream = require('../../index.js');
 var test = require('tape');
 var fs = require('fs');
 var streamEqual = require('stream-equal');
-
-test('a very large binary file piped into unbzip2-stream results in original file content', function(t) {
+test('a very large binary file piped into unbzip2-stream results in original file content', function (t) {
     t.plan(1);
     var source = fs.createReadStream('test/fixtures/vmlinux.bin.bz2');
     var expected = fs.createReadStream('test/fixtures/vmlinux.bin');
     var unbz2 = unbzip2Stream();
     source.pipe(unbz2);
-    streamEqual(expected, unbz2, function(err, equal) {
+    streamEqual(expected, unbz2, function (err, equal) {
         if (err)
             t.ok(false, err);
-        t.ok(equal, "same file contents");
+        t.ok(equal, 'same file contents');
     });
 });
